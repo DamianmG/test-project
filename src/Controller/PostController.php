0a23ed84cda34a74445d6f5b59f815de22fbffb0
Damian Gdula <damian@Damians-Air.fios-router.home>
2023-12-12 19:18:13 -0500

    Removing posts added

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,9 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PostController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/lista', name: 'post_list')]
     public function postList(PostRepository $postRepository): Response 
     {
@@ -17,5 +24,18 @@ class PostController extends AbstractController
         return $this->render('post/post_list.html.twig', [
             'posts' => $posts,
         ]);
+    }
+
+    #[Route('/delete-post/{postId}', methods:['GET', 'DELETE'], name: 'delete_post')]
+    public function delete(PostRepository $postRepository, $postId): Response
+    {
+        $post = $postRepository->find($postId);
+        
+        $this->entityManager->remove($post);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'Post deleted successfully.');
+
+        return $this->redirectToRoute('post_list');
     }
 }
